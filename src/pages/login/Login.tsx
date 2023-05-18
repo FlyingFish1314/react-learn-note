b12ff87639e3dc943fbbb188833df099e1c71cfb
FlyingFish1314 <79733953+FlyingFish1314@users.noreply.github.com>
2023-05-18 22:04:29 +0800

    feat: 登录注册基本样式与校验

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,16 +1,106 @@
-import React, { FC } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
+import React, { FC, useEffect } from 'react'
+import { Typography, Space, Form, Input, Button, Checkbox } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import styles from './Login.module.scss'
+import { Link } from 'react-router-dom'
 
-const Login: FC = () => {
-  const nav = useNavigate()
+const { Title } = Typography
 
+const USERNAME_KEY = 'USERNAME'
+const PASSWORD_KEY = 'PASSWORD'
+
+function rememberUser(username: string, password: string) {
+  localStorage.setItem(USERNAME_KEY, username)
+  localStorage.setItem(PASSWORD_KEY, password)
+}
+function forgetUser() {
+  localStorage.removeItem(USERNAME_KEY)
+  localStorage.removeItem(PASSWORD_KEY)
+}
+function getUserInfoFromStorage() {
+  return {
+    username: localStorage.getItem(USERNAME_KEY),
+    password: localStorage.getItem(PASSWORD_KEY)
+  }
+}
+
+const Register: FC = () => {
+  const [form] = Form.useForm()
+  useEffect(() => {
+    const { username, password } = getUserInfoFromStorage()
+    form.setFieldsValue({ username, password })
+  }, [])
+
+  function onFinish(values: any) {
+    const { username, password, remember } = values || {}
+    if (remember) {
+      rememberUser(username, password)
+    } else {
+      forgetUser()
+    }
+    console.log('🚀 ~ file: Register.tsx:10 ~ onFinish ~ value:', values)
+  }
   return (
-    <div>
-      <p>login</p>
-      <button onClick={() => nav(-1)}>返回</button>
-      <Link to="/register">注册</Link>
+    <div className={styles.container}>
+      <div>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined></UserAddOutlined>
+          </Title>
+          <Title level={2}>登录</Title>
+        </Space>
+      </div>
+      <div>
+        <Form
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 20 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          form={form}
+        >
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              { required: true, message: '请输入用户名' },
+              {
+                type: 'string',
+                min: 5,
+                max: 20,
+                message: '字符长度在5-20之间'
+              },
+              { pattern: /^\w+$/, message: '只能是字母数字下划线' }
+            ]}
+          >
+            <Input></Input>
+          </Form.Item>
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[{ required: true, message: '请输入密码' }]}
+          >
+            <Input.Password></Input.Password>
+          </Form.Item>
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ offset: 6, span: 20 }}
+          >
+            <Checkbox>记住我</Checkbox>
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 6, span: 20 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                登录
+              </Button>
+              <Link to="/register">没有账号，去注册</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   )
 }
 
-export default Login
+export default Register
--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,7 +1,86 @@
 import React, { FC } from 'react'
+import { Typography, Space, Form, Input, Button } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import styles from './Register.module.scss'
+import { Link } from 'react-router-dom'
 
+const { Title } = Typography
 const Register: FC = () => {
-  return <div>Register</div>
+  function onFinish(value: any) {
+    console.log('🚀 ~ file: Register.tsx:10 ~ onFinish ~ value:', value)
+  }
+  return (
+    <div className={styles.container}>
+      <div>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined></UserAddOutlined>
+          </Title>
+          <Title level={2}>注册新用户</Title>
+        </Space>
+      </div>
+      <div>
+        <Form
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 20 }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              { required: true, message: '请输入用户名' },
+              {
+                type: 'string',
+                min: 5,
+                max: 20,
+                message: '字符长度在5-20之间'
+              },
+              { pattern: /^\w+$/, message: '只能是字母数字下划线' }
+            ]}
+          >
+            <Input></Input>
+          </Form.Item>
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[{ required: true, message: '请输入密码' }]}
+          >
+            <Input.Password></Input.Password>
+          </Form.Item>
+          <Form.Item
+            label="确认密码"
+            name="confirm"
+            dependencies={['password']}
+            rules={[
+              { required: true, message: '请输入密码' },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve()
+                  }
+                  return Promise.reject(new Error('两次输入密码不一致'))
+                }
+              })
+            ]}
+          >
+            <Input.Password></Input.Password>
+          </Form.Item>
+          <Form.Item label="昵称" name="nickname">
+            <Input></Input>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 20 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                注册
+              </Button>
+              <Link to="/login">已有账号，请登录</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  )
 }
 
 export default Register
