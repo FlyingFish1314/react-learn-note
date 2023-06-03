a79d6dd3efeb1205fd89180e3681af5ae6dd5827
FlyingFish1314 <79733953+FlyingFish1314@users.noreply.github.com>
2023-06-03 21:37:58 +0800

    feat: useRequest疯转ajax请求

--- src/hooks/useLoadQuestionData.ts
+++ src/hooks/useLoadQuestionData.ts
@@ -1,25 +1,33 @@
 import { getQuestionService } from '@/service/question'
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
+import { useRequest } from 'ahooks'
+
 import { useParams } from 'react-router-dom'
 
 function useLoadQuestionData() {
   const { id = '' } = useParams()
-  const [loading, setLoading] = useState(true)
-  const [questionData, setQuestionData] = useState({})
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getQuestionService(id)
-        setQuestionData(data)
-        setLoading(false)
-      } catch (error) {
-        console.log('🚀 ~ file: index.tsx:12 ~ fetchData ~ error:', error)
-      }
-    }
-    fetchData()
-  }, [])
+  // const [loading, setLoading] = useState(true)
+  // const [questionData, setQuestionData] = useState({})
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const data = await getQuestionService(id)
+  //       setQuestionData(data)
+  //       setLoading(false)
+  //     } catch (error) {
+  //       console.log('🚀 ~ file: index.tsx:12 ~ fetchData ~ error:', error)
+  //     }
+  //   }
+  //   fetchData()
+  // }, [])
 
-  return { loading, questionData }
+  async function load() {
+    const data = await getQuestionService(id)
+    return data
+  }
+  const { loading, data, error } = useRequest(load)
+
+  return { loading, data, error }
 }
 
 export default useLoadQuestionData
--- src/pages/question/stat/index.tsx
+++ src/pages/question/stat/index.tsx
@@ -3,12 +3,12 @@ import React, { FC } from 'react'
 import useLoadQuestionData from '@/hooks/useLoadQuestionData'
 
 const Stat: FC = () => {
-  const { loading, questionData } = useLoadQuestionData()
+  const { loading, data } = useLoadQuestionData()
 
   return (
     <div>
       <p>Stat page</p>
-      {loading ? <p>loading</p> : <p>{JSON.stringify(questionData)}</p>}
+      {loading ? <p>loading</p> : <p>{JSON.stringify(data)}</p>}
     </div>
   )
 }
